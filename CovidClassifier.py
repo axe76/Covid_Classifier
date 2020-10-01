b7300920f48788bd80e287dc3673f830374cc091
Atharva Hudlikar <atharvahudlikar@gmail.com>
2020-10-01 12:13:27 +0530

    Update CovidClassifier.py

--- CovidClassifier.py
+++ CovidClassifier.py
@@ -31,7 +31,7 @@ validation_set = test_dataset.flow_from_directory(r"C:\Users\ACER\Desktop\AI\Cov
 
 model.fit_generator(training_set,steps_per_epoch = 8,epochs = 10,validation_data = validation_set,validation_steps = 2)
 
-model.save("covid_model.h5")
+model.save("model.h5")
 
 test_image = image.load_img(r"C:\Users\ACER\Desktop\AI\Covid_Xray\Covid-19-Detection-master\CovidDataset\Val\Normal\NORMAL2-IM-0730-0001.jpeg", target_size = (224,224))
 test_image = image.img_to_array(test_image)
